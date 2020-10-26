14f12936fc4295e2395b6dc6d54b615bd1f89934
brajeshrai <brajesh.rai@pfizer.com>
2020-10-26 12:30:50 -0400

    first implementation of the symmetry function code

--- torsion/analysis/__init__.py
+++ torsion/analysis/__init__.py
@@ -2,4 +2,6 @@ from .analysis import (
     construct_dihedral_energy_profile,
     extract_molecule_torsion_data,
     cal_molecule_torsion_strain,
+    get_dihedral_inchi_key,
+    get_specific_dihedral_inchi_key
 )
